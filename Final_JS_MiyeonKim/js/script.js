e1ccca8295dd3355ba23abd902108861bdefcdee
kimm068 <kimm068@newschool.edu>
2015-10-01 18:49:07 -0400

    JS Final

---
+++ Final_JS_MiyeonKim/js/script.js
@@ -0,0 +1,14 @@
+// // console.log("dfsgfsdfg");
+
+// // var img01;
+
+
+// var title;
+
+
+// $(document).ready(function(){
+// 	$('#title1').click(function(){
+// 		$('#title1').fadeOut('slow');
+// 		console.log('fadeout');
+// 	});
+// });
